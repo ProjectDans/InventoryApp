199790b3fc8704ee4882d213df02abae3a02b060
Daniel Celo <12113268>
2022-12-08 03:39:55 +0700

    First commit

---
+++ app/src/main/java/com/danscoding/inventoryapp/DaftarPelangganActivity.kt
@@ -0,0 +1,11 @@
+package com.danscoding.inventoryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DaftarPelangganActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_pelanggan)
+    }
+}
---
+++ app/src/main/java/com/danscoding/inventoryapp/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.danscoding.inventoryapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.danscoding.inventoryapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
